4eb2bcf6095eea0896a2044c00422b40dece7d2d
Bing <ybingcheng@gmail.com>
2018-01-10 17:54:02 +0800

    Improved
    Revised neighbor comparison, removed heuristic dictionary

--- student_code.py
+++ student_code.py
@@ -6,15 +6,13 @@ def shortest_path(M,start,goal):
     explored = set()
     parent = {}
     point_gcost = {}
-    point_hcost = {}
     point_fcost = {}
     
     # Initialise current
     current = start
     frontier.add(current)
     point_gcost[current] = 0
-    point_hcost[current] = cost(M, start, goal)
-    point_fcost[current] = point_gcost[current] + point_hcost[current]
+    point_fcost[current] = point_gcost[current] + cost(M, start, goal)
     parent[current] = None
  
     #while there is possible path in frontier
@@ -38,22 +36,20 @@ def shortest_path(M,start,goal):
         # neighbour checks
         for node in M.roads[current]:
             if node in explored:
-                continue
-            if node in frontier:
-                new_gcost = point_gcost[current] + cost(M, node, current)
-                # compare and update gcost 
-                if point_gcost[node] > new_gcost:
-                    point_gcost[node] = new_gcost
-                    point_fcost[node] = point_gcost[node] + point_hcost[node]
-                    parent[node] = current
-            else:
-                # calculate fcost/gcost/hcost and parent
-                point_gcost[node] = point_gcost[current] + cost(M, node, current)
-                point_hcost[node] = cost(M, node, goal)
-                point_fcost[node] = point_gcost[node] + point_hcost[node]
-                parent[node] = current
+                continue  # Evaluated
+                
+            if node not in frontier:
                 frontier.add(node)
-
+                point_gcost[node] = point_gcost[current] + cost(M, node, current)
+                point_fcost[node] = point_gcost[node] + cost(M, node, goal)
+                parent[node] = current
+                
+            new_gcost = point_gcost[current] + cost(M, node, current)
+            
+            if point_gcost[node] > new_gcost :
+                point_gcost[node] = new_gcost
+                point_fcost[node] = point_gcost[node] + cost(M, node, goal)
+                parent[node] = current                
                         
 def cost(M, node1, node2):
     nodeone = M.intersections[node1]
